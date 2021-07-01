e3d2d2f29cdba0d8c17ae7e53eb1ec89ef99cdfa
tensaurus <tensaurus@users.noreply.github.com>
2021-07-01 16:58:55 +0530

    keep ship on the screen

--- ship.py
+++ ship.py
@@ -23,9 +23,9 @@ class Ship:
         self.screen.blit(self.image, self.rect)
 
     def update_position(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         # Update rect object from self.x
         self.rect.x = self.x
